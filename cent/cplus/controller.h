5ee2e24f42388e478c8b3ee569796d1db7f3ce04
scyo. Lê Trọng Nghĩa <120289465+SakateKataro@users.noreply.github.com>
2025-02-28 17:01:17 +0700

    A000011 - add linked in controller

--- cent/cplus/controller.h
+++ cent/cplus/controller.h
@@ -3,9 +3,20 @@
 #include <string>
 
 
+// Controller - Service - Module - Package
+#include "cpplib.h"
 
 
 // Class definition
 class Controller {
-    
+    private:
+        std::string _module;
+        std::string _service;
+        std::string _package;
+        std::string _controller;
+        std::string _path;
+        std::string _file;
+        std::string _content;
+        std::string _template;
+
 };
